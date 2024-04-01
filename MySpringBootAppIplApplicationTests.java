63cdbf11fb827a055c2ce12845507dc68640b699
NithinPanchumarthi636 <155046948+NithinPanchumarthi636@users.noreply.github.com>
2024-04-01 15:20:53 +0530

    Add files via upload

---
+++ MySpringBootAppIplApplicationTests.java
@@ -0,0 +1,6 @@
+/*
+ * //package com.rs.myspringbootapp; // //import
+ * org.springframework.boot.SpringApplication; //import
+ * org.springframework.boot.test.context.SpringBootTest; // // //@SpringBootTest
+ * //class MySpringBootAppIplApplicationTests { // // //}
+ */
